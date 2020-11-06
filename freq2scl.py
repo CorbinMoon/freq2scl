055b667eb11e82f35f7c5cf7f1516e3f138c262b
CorbinMoon <clangmoon@gmail.com>
2020-11-06 00:48:02 -0500

    put dis method in class

--- freq2scl.py
+++ freq2scl.py
@@ -9,37 +9,44 @@ _a = -3.5
 _b = -5.75
 
 
-def dis(f, amp):
-    idx = np.argsort(f)
-    s_amp = np.asarray(amp)[idx]
-    s_f = np.asarray(f)[idx]
-    idx = np.transpose(np.triu_indices(len(s_f), 1))
-
-    f_pairs = s_f[idx]
-    amp_pairs = s_amp[idx]
-
-    f_min = f_pairs[:, 0]
-    s = d / (s_1 * f_min + s_2)
-    x = s * (f_pairs[:, 1] - f_pairs[:, 0])
-
-    v = np.prod(amp_pairs, axis=1)
-
-    return np.sum(v * (np.exp(_a * x) - np.exp(_b * x)))
-
-
 class Freq2Scl(object):
 
     def __init__(self):
         self.__data = None
         self.__alpharange = None
 
+    @classmethod
+    def __dis(cls, f, amp):
+        idx = np.argsort(f)
+        s_amp = np.asarray(amp)[idx]
+        s_f = np.asarray(f)[idx]
+        idx = np.transpose(np.triu_indices(len(s_f), 1))
+
+        f_pairs = s_f[idx]
+        amp_pairs = s_amp[idx]
+
+        f_min = f_pairs[:, 0]
+        s = d / (s_1 * f_min + s_2)
+        x = s * (f_pairs[:, 1] - f_pairs[:, 0])
+
+        v = np.prod(amp_pairs, axis=1)
+
+        return np.sum(v * (np.exp(_a * x) - np.exp(_b * x)))
+
+    @property
+    def get_data(self):
+        return self.__data
+
     def calc_dis(self, f, amp, n=2000, alpharange=2.1):
+        if alpharange <= 1:
+            raise Exception("alpharange must be greater than one")
+
         data = np.zeros(n, dtype=np.float)
         a = np.concatenate((amp, amp))
 
         for i, alpha in enumerate(np.linspace(1, alpharange, n)):
             freq = np.concatenate((f, alpha * f))
-            data[i] = dis(freq, a)
+            data[i] = self.__dis(freq, a)
 
         self.__data = data
         self.__alpharange = alpharange
@@ -48,6 +55,6 @@ class Freq2Scl(object):
         plt.plot(np.linspace(1, self.__alpharange,
                              len(self.__data)), self.__data)
         plt.xlim(1, self.__alpharange)
-        plt.xlabel('frequency ratio')
+        plt.xlabel('freq ratio')
         plt.ylabel('sensory dissonance')
         plt.show()
